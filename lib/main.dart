c91dc9983205de8dd946d685abd8c8fa59e6ee2f
kishan kumar <sbittu348@gmail.com>
2025-10-27 11:38:59 +0530

    changes in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travaly_app/app.dart';
 import 'package:travaly_app/core/dependency_injection/service_locator.dart';
-import 'package:travaly_app/firebase_options.dart';
 
-void main() async  {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-   
+  await Firebase.initializeApp();
+
   setupDependencies();
   runApp(const HotelBookingApp());
 }
