1e3a69153bba36743849c9c37e7cdea177c8ea73
kishan kumar <sbittu348@gmail.com>
2025-10-27 12:05:01 +0530

    secret keys shifted to .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:travaly_app/app.dart';
 import 'package:travaly_app/core/dependency_injection/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+      await dotenv.load();
   await Firebase.initializeApp();
-
   setupDependencies();
   runApp(const HotelBookingApp());
 }
